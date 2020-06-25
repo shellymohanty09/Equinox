3b7f28ce22c02fd7580865815dae32f39949918e
shellymohanty09 <59158166+shellymohanty09@users.noreply.github.com>
2020-06-25 20:32:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".type", {
+  strings: [
+    "You can't solve a problem on the same level that it was created.",
+    "The creation of something new is not accomplished by the intellect, but by the play instinct arising from inner necessity.",
+    "In the beginner's mind there are many possibilities; in the expert's mind there are few."
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop:true
+});
+
+// $(function () {
+//             $(document).scroll(function () {
+//                 var $nav = $("#mainNavbar");
+//                 $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//             });
+//         });
